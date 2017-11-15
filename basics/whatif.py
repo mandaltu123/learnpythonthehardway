8d06b2e1f0bb11d59e41933f2c6d3bc19f1af2ae
tuhinmandal <myid.tuhin123@gmail.com>
2017-11-15 20:37:05 +0530

    some if statements

---
+++ basics/whatif.py
@@ -0,0 +1,34 @@
+# This will demonstrate simple if clauses
+
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats ! The world will be doomed")
+
+
+if people > cats:
+    print("Not many cats, the world is saved")
+
+
+if people < dogs:
+    print("The world is drooled on")
+
+
+if people > dogs:
+    print("The world is dry")
+
+
+dogs += 5
+if people >= dogs:
+    print("people are greater or equal to dogs!")
+
+if people <= dogs:
+    print("people are less than or equal to dogs")
+
+if people == dogs:
+    print("people are equals to dogs")
+
+
