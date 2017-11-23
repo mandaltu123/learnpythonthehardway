7a4ebbdfb2a7bcea9483551ec0b53c98a97ae419
tuhinmandal <myid.tuhin123@gmail.com>
2017-11-23 19:14:45 +0530

    adding dictionaries

---
+++ basics/dictionaries.py
@@ -0,0 +1,44 @@
+# Dictionaries are the most commonly used datastructure in python
+# They are key value pairs
+
+
+dic = {1: 'one', 2: 'two', 3: 'three'}
+
+print("print my first dictionary {}".format(dic)) # I did not write dick
+
+# it has keys and values
+keys = dic.keys()
+values = dic.values()
+
+print("keys are {}".format(keys))
+print("values are {}".format(values))
+
+# one way of iteration over dictionary is :
+for key in keys:
+    print("key = {} and value = {}".format(key, dic.get(key)))
+
+
+
+# item() function in dictionary
+items = dic.items()
+for item, data in dic.items():
+    print("the key is {} and the value is {}".format(item, dic.get(item)))
+
+
+# adding a new item to dictionary
+dic[4] = 'four'
+print("new dictionary is {} ".format(dic))
+
+
+# adding a new item with same key
+dic[4] = 'iv'
+print("adding another item with same key 4 {}".format(dic)) # if you are coming from java background it does the same
+# same as like hash table or hashmap it overwrites the old item with the new one
+
+
+
+# del operator
+
+del  dic[4]
+print("dictionary after deleting 4 {}".format(dic))
+
--- basics/readme.md
+++ basics/readme.md
@@ -1,1 +1,1 @@
-All the practice pieces of code are read from the fantastic book learn python the hard way
+Most of the practice pieces of code are read from the fantastic book learn python the hard way
