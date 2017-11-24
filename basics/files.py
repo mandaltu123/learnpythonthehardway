235e828999bf561da23ce349c9cdc4899acd0e49
tuhinmandal <myid.tuhin123@gmail.com>
2017-11-24 12:53:17 +0530

    adding read write to files

--- basics/files.py
+++ basics/files.py
@@ -2,5 +2,14 @@
 
 input_file = input("please input the file name >> ")
 file = open(input_file, 'r') # r is read mode. here we are opening the file in read mode
-print(file.read())
+data = file.read()
+print("data in the file \n{}".format(data))
 file.close()
+
+# The next section will be about writing something into a file
+
+output_file = input("please provide a file name where you want to white the data >> ")
+out_file = open(output_file, 'w')
+out_file.write(data)
+out_file.close()
+print("writing to {} is done".format(output_file))
